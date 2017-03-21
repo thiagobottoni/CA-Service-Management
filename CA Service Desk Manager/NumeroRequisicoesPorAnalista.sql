1eeb49e5bd14fd49401bfff06b9d85c4bc736953
Thiago Bottoni <bottonithiago@gmail.com>
2017-03-21 16:44:16 -0300

    Create NumeroRequisicoesPorAnalista.sql

---
+++ CA Service Desk Manager/NumeroRequisicoesPorAnalista.sql
@@ -0,0 +1,7 @@
+SELECT	con.alias, COUNT(req.id)
+FROM		call_req req
+INNER JOIN  ca_contact con
+ON req.assignee = con.contact_uuid
+WHERE DATEADD(ss, open_date, '19700101') > '20170101'
+AND DATEADD(ss, open_date, '19700101') < '20170201'
+GROUP BY con.alias
