d33da8236ee9f67e22dca0638c0db402efc32fa8
Thiago Bottoni <bottonithiago@gmail.com>
2017-03-21 16:45:26 -0300

    Update BuscaCamposDeFormulario.sql

--- CA Service Catalalog/BuscaCamposDeFormulario.sql
+++ CA Service Catalalog/BuscaCamposDeFormulario.sql
@@ -1,8 +1,7 @@
-SELECT	form_elem_name
-		    , form_elem_value
-FROM	      usm_request_item_form 
-	LEFT JOIN	usm_subscription_detail 
-			   ON	subscription_detail_id = usm_subscription_detail.id 
+SELECT form_elem_name, form_elem_value
+FROM usm_request_item_form 
+LEFT JOIN usm_subscription_detail 
+ON subscription_detail_id = usm_subscription_detail.id 
 WHERE request_id = '10574' --Altere pelo código da sua requisição
 AND subscription_type = 5
 AND is_visible = 1
