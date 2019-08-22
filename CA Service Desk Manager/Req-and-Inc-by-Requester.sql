a1b673a72401c56ac3efa08fa740e3c3e2eb5137
Thiago Bottoni <bottonithiago@gmail.com>
2019-08-22 15:25:36 -0300

    Update and rename ListaReqIncPorRequisitante.sql to Req-and-Inc-by-Requester.sql

--- CA Service Desk Manager/Req-and-Inc-by-Requester.sql
+++ CA Service Desk Manager/Req-and-Inc-by-Requester.sql
@@ -1,11 +1,11 @@
 SELECT top 15 req.id, req.persid, req.summary, con.userid, req.open_date,
   CASE req.type
-      when 'I' then 'Incidente'   
-      when 'R' then 'Requisição'
+      when 'I' then 'Incident'   
+      when 'R' then 'Request'
   end as type
 FROM call_req req
   INNER JOIN  ca_contact con
     ON req.customer = con.contact_uuid
 WHERE req.type IN ('I','R')
-  AND con.userid = 'jbrown' -- ID do usuário
+  AND con.userid = 'jbrown' -- User ID
 ORDER BY req.open_date DESC
