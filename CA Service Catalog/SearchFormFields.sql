eff2909fd76f02408ab6453eacd07f0ef71e625c
Thiago Bottoni <bottonithiago@gmail.com>
2019-08-22 15:20:34 -0300

    Update and rename BuscaCamposDeFormulario.sql to SearchFormFields.sql

--- CA Service Catalog/SearchFormFields.sql
+++ CA Service Catalog/SearchFormFields.sql
@@ -2,7 +2,7 @@ SELECT form_elem_name, form_elem_value
 FROM usm_request_item_form 
 LEFT JOIN usm_subscription_detail 
 ON subscription_detail_id = usm_subscription_detail.id 
-WHERE request_id = '10574' --Altere pelo código da sua requisição
+WHERE request_id = '10574' --Replace with a valid Request ID
 AND subscription_type = 5
 AND is_visible = 1
 ORDER BY request_id
